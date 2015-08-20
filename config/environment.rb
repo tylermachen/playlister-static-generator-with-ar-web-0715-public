d714f59233a4335369000fb6b343747c332e78c6
Tyler Machen <tylermachen@gmail.com>
2015-08-20 18:55:46 -0400

    complete playlister generator

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require 'bundler/setup'
+require 'fileutils'
 Bundler.require
 
 ENV["PLAYLISTER_ENV"] ||= "development"
@@ -20,7 +21,7 @@ def migrate_db
     DB.execute("DROP TABLE #{table}")
   end
 
-  Dir[File.join(File.dirname(__FILE__), "../db/migrations", "*.rb")].each do |f| 
+  Dir[File.join(File.dirname(__FILE__), "../db/migrations", "*.rb")].each do |f|
     require f
     migration = Kernel.const_get(f.split("/").last.split(".rb").first.gsub(/\d+/, "").split("_").collect{|w| w.strip.capitalize}.join())
     migration.migrate(:up)
--- lib/support/site_generator.rb
+++ lib/support/site_generator.rb
@@ -4,4 +4,66 @@ class SiteGenerator
   def initialize(url)
     @rendered_path = url
   end
+
+  def build_index
+    t = File.read("app/views/index.html.erb")
+    FileUtils::mkdir_p "#{rendered_path}"
+    File.open("#{@rendered_path}/index.html", "w+") do |f|
+      f.puts ERB.new(t).result(binding)
+    end
+  end
+
+  def build_artists_index
+    t = File.read("app/views/artists/index.html.erb")
+    FileUtils::mkdir_p "#{rendered_path}/artists" # create directories
+    File.open("#{@rendered_path}/artists/index.html", "w+") do |f|
+      f.puts ERB.new(t).result(binding)
+    end
+  end
+
+  def build_artist_page
+    t = File.read("app/views/artists/show.html.erb")
+    Artist.all.each do |artist|
+      @artist = artist # not sure why this is asked for in tests
+      File.open("#{@rendered_path}/artists/#{artist.to_slug}.html", "w+") do |f|
+        f.puts ERB.new(t).result(binding)
+      end
+    end
+  end
+
+  def build_genres_index
+    t = File.read("app/views/genres/index.html.erb")
+    FileUtils::mkdir_p "#{rendered_path}/genres" # create directories
+    File.open("#{@rendered_path}/genres/index.html", "w+") do |f|
+      f.puts ERB.new(t).result(binding)
+    end
+  end
+
+  def build_genre_page
+    t = File.read("app/views/genres/show.html.erb")
+    Genre.all.each do |genre|
+      @genre = genre # not sure why this is asked for in tests
+      File.open("#{@rendered_path}/genres/#{genre.to_slug}.html", "w+") do |f|
+        f.puts ERB.new(t).result(binding)
+      end
+    end
+  end
+
+  def build_songs_index
+    t = File.read("app/views/songs/index.html.erb")
+    FileUtils::mkdir_p "#{rendered_path}/songs" # create directories
+    File.open("#{@rendered_path}/songs/index.html", "w+") do |f|
+      f.puts ERB.new(t).result(binding)
+    end
+  end
+
+  def build_song_page
+    t = File.read("app/views/songs/show.html.erb")
+    Song.all.each do |song|
+      @song = song # not sure why this is asked for in tests
+      File.open("#{@rendered_path}/songs/#{song.to_slug}.html", "w+") do |f|
+        f.puts ERB.new(t).result(binding)
+      end
+    end
+  end
 end
